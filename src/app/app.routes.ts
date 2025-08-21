1561c7bf01df1bd1a812e59992b61867b58b17af
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-08-21 12:06:31 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/header/home/home/home';
+import { AboutComponent } from './components/header/about/about/about';
+import{ProjectsComponent} from './components/header/projects/projects/projects';
+import { ContactComponent } from './components/header/contact/contact/contact';
 
-export const routes: Routes = [];
+
+ export const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'projects', component: ProjectsComponent },
+{ path: 'contact', component: ContactComponent},
+{ path: '**', redirectTo: '' }
+];
