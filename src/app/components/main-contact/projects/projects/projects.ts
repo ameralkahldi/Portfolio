41afa259368abd6402ab13364e790182aa3588ac
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-08-25 16:22:45 +0200

    Add skills Seites

--- src/app/components/main-contact/projects/projects/projects.ts
+++ src/app/components/main-contact/projects/projects/projects.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './projects.scss'
 })
 export class ProjectsComponent {
+projects : {
+  title?: string;
+  description?: string;
+  image ?: string;
+  link ?: string;
+  testLink ?: string;
+}[] = [
+  {
+    title: 'Pekomen Show',
+    description: 'JavaScript, CSS, HTML, and API',
+    image: 'assets/img/2025-08-25 (1).png',
+    link: 'https://amer-alkhalidy.developerakademie.net/Pokemon%20API/Pokemon%20API/index.html',
+    testLink: 'https://amer-alkhalidy.developerakademie.net/Pokemon%20API/index.html'
+  },
+  {
+    title: 'El Pollo Loco Spiel',
+    description: 'JavaScript, CSS, and HTML',
+    image: 'assets/img/2025-08-25 (3).png',
+    link: 'https://github.com/ameralkahldi/El-Pollo-Loco',
+    testLink: 'https://amer-alkhalidy.developerakademie.net/EL%20Pollo%20Loco/index.html'
+  },
+  {
+    title: 'Project 3',
+    description: 'Coming soon...',
+    image: '',      
+    link: '',
+    testLink: ''
+  }
+];
 
 }
