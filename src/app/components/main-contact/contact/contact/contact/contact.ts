05e17332d12dd17d3f755ade8a890befa88f14cd
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-08-26 14:26:06 +0200

    Add Edite for contact and project Seites

--- src/app/components/main-contact/contact/contact/contact/contact.ts
+++ src/app/components/main-contact/contact/contact/contact/contact.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      privacy: [false, Validators.requiredTrue]
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      this.submitted = true;
+      console.log(this.contactForm.value);
+      this.contactForm.reset();
+    }
+  }
 }
--- src/app/components/main-contact/projects/projects/projects.ts
+++ src/app/components/main-contact/projects/projects/projects.ts
@@ -5,22 +5,24 @@ import { Component } from '@angular/core';
   selector: 'app-projects',
   standalone: true,
   templateUrl: './projects.html',
-  styleUrl: './projects.scss',
-  imports: [CommonModule]   
+  styleUrls: ['./projects.scss'],  
+  imports: [CommonModule]
 })
 export class ProjectsComponent {
   projects = [
     {
       title: 'Pekomen Show',
-      description: 'JavaScript, CSS, HTML, and API',
+      description: 'A dynamic website showcasing delicious chicken recipes.',
       image: 'assets/img/2025-08-25 (1).png',
+      techStack: ['JavaScript', 'CSS', 'HTML', 'API'],
       link: 'https://amer-alkhalidy.developerakademie.net/Pokemon%20API/Pokemon%20API/index.html',
       testLink: 'https://amer-alkhalidy.developerakademie.net/Pokemon%20API/index.html'
     },
     {
       title: 'El Pollo Loco Spiel',
-      description: 'JavaScript, CSS, and HTML',
+      description: 'A dynamic website showcasing delicious chicken recipes.',
       image: 'assets/img/2025-08-25 (3).png',
+      techStack: ['JavaScript', 'CSS', 'HTML'],
       link: 'https://github.com/ameralkahldi/El-Pollo-Loco',
       testLink: 'https://amer-alkhalidy.developerakademie.net/EL%20Pollo%20Loco/index.html'
     },
@@ -28,22 +30,45 @@ export class ProjectsComponent {
       title: 'Project 3',
       description: 'Coming soon...',
       image: 'assets/img/coming-soon.png',
+      techStack: ['JavaScript', 'CSS', 'HTML'],
       link: '',
       testLink: ''
     }
   ];
 
-  selectedProjectIndex = 0;
+  selectedProjectIndex: number | null = null;
 
   get selectedProject() {
+    if (this.selectedProjectIndex === null) return null;
     return this.projects[this.selectedProjectIndex];
   }
 
   selectProject(index: number) {
-    this.selectedProjectIndex = index;
+    if (this.selectedProjectIndex === index) {
+      this.selectedProjectIndex = null;
+    } else {
+      this.selectedProjectIndex = index;
+    }
   }
-  trackByTitle(index: number, project: any): string {
-  return project.title;
-}
 
+  trackByTitle(index: number, project: any): string {
+    return project.title;
+  }
+   prevProject() {
+    if (this.selectedProjectIndex === null) return;
+    if (this.selectedProjectIndex === 0) {
+      this.selectedProjectIndex = this.projects.length - 1; 
+    } else {
+      this.selectedProjectIndex--;
+    }
+  }
+
+  nextProject() {
+    if (this.selectedProjectIndex === null) return;
+    if (this.selectedProjectIndex === this.projects.length - 1) {
+      this.selectedProjectIndex = 0; 
+    } else {
+      this.selectedProjectIndex++;
+    }
+  }
 }
