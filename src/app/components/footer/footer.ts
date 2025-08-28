219ff9dc13d97247306b1a7c985513c429d2f045
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-08-28 15:28:01 +0200

    Add edite foe skill ,main-contect and footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ImprintComponent } from './imprint/imprint';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ CommonModule ,ImprintComponent,RouterModule],
+  imports: [ CommonModule ,RouterModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.scss'],
 })
