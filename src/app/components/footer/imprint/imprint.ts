423076e7c473e6704e8e44cc53232763c93d8e8c
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-03 18:31:13 +0200

    add back button for imprint page

--- src/app/components/footer/imprint/imprint.ts
+++ src/app/components/footer/imprint/imprint.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-imprint',
-  imports: [CommonModule,TranslatePipe],
+  standalone: true, 
+  imports: [CommonModule, TranslatePipe, RouterModule],
   templateUrl: './imprint.html',
   styleUrls: ['./imprint.scss']
 })
-export class ImprintComponent {
-
-}
+export class ImprintComponent {}
