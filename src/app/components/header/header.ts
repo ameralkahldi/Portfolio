d3ba61542bb4b459be04d198de88d96647f92c40
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-08 12:12:10 +0200

    Add edite for style

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,26 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core'; // ✅ استيراد الخدمة
+import { TranslatePipe, TranslateService } from '@ngx-translate/core'; 
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
-  imports: [CommonModule, TranslatePipe] // ✅ فقط الموديولات أو المكونات أو الأنابيب
+  imports: [CommonModule, TranslatePipe] 
 })
 export class HeaderComponent implements OnInit {
   currentLang: string = 'en';
   isMobileView: boolean = false;
   isMenuVisible: boolean = false;
 
-  constructor(private translate: TranslateService) {} // ✅ حقن الخدمة
+  constructor(private translate: TranslateService) {} 
 
   ngOnInit(): void {
     const savedLang = localStorage.getItem('lang');
     if (savedLang) {
       this.currentLang = savedLang;
-      this.translate.use(savedLang); // ✅ تعيين اللغة المحفوظة
+      this.translate.use(savedLang); 
     }
 
     this.checkScreenWidth(window.innerWidth);
@@ -57,4 +57,11 @@ export class HeaderComponent implements OnInit {
   closeMenu() {
     this.isMenuVisible = false;
   }
+  activeLink: string = '';
+
+setActive(link: string) {
+  this.activeLink = link;
+  this.closeMenu();
+}
+
 }
