18409a325576c00c67a82b410c27be7abc353fc4
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-11 08:33:40 +0200

    add edite for image skills

--- src/app/components/main-contact/skills/skills.ts
+++ src/app/components/main-contact/skills/skills.ts
@@ -12,48 +12,62 @@ export class SkillsComponent {
   skills :{
  
     icon: string;
+    text :string
+    
   }[] = [
     {
     
-      icon : 'assets/icon_logos/HTML.png'
+      icon : 'assets/icon_logos/html.png',
+      text : 'HTML'
     },
     {
     
-      icon : 'assets/icon_logos/17. Skill animation.png'
-    },
-    {
-      
-      icon : 'assets/icon_logos/JavaScript.png'
-    },
-    {
-
-      icon : 'assets/icon_logos/Material Design (1).png'
-    },
-    {
-     
-      icon : 'assets/icon_logos/TypeScript.png'
-    },
-    {
-     
-      icon : 'assets/icon_logos/Angular.png'
-    },
-    {
-      icon : 'assets/icon_logos/Firebase.png'
+      icon : 'assets/icon_logos/16. Tech icons.png',
+       text : 'CSS',
 
     },
     {
       
-      icon : 'assets/icon_logos/GIT.png'
+      icon : 'assets/icon_logos/Javascript.png',
+       text : 'JavaScript'
+    },
+    {
+
+      icon : 'assets/icon_logos/Frame.png',
+       text : 'Material Design'
+    },
+    {
+     
+      icon : 'assets/icon_logos/Vector (1).png',
+       text : 'TypeScript',
+    },
+    {
+     
+      icon : 'assets/icon_logos/Angular.png',
+       text : 'Angular'
+    },
+    {
+      icon : 'assets/icon_logos/16. Tech icons (1).png',
+       text : 'Firebase'
+
+    },
+    {
+      
+      icon : 'assets/icon_logos/16. Tech icons (2).png',
+       text : 'Git'
     }
     ,{
       
-      icon : 'assets/icon_logos/Rest-Api.png'
+      icon : 'assets/icon_logos/Api.png',
+       text : 'REST-API'
     },
     {
-      icon :'assets/icon_logos/Scrum.png'
+      icon :'assets/icon_logos/16. Tech icons (3).png',
+       text : 'Scrum'
     },
     {
-      icon:'assets/icon_logos/18. Growth Mindset interaction.png'
+      icon:'assets/icon_logos/16. Tech icons (4).png',
+       text : 'Growth mindset'
     }
   ]
 
