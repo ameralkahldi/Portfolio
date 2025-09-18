dcb5ca69004668a98c83eb5f1b2d5c11026c627f
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-18 22:10:19 +0200

    Add Privacy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ImprintComponent } from './components/footer/imprint/imprint';
 import { MainContactComponent } from './components/main-contact/main-contact';
+import { PrivacyPolicy } from './components/main-contact/privacy/privacy-policy';
 
 
 
  export const routes: Routes = [
 { path: '', component: MainContactComponent },
 { path: 'legal', component: ImprintComponent },
+{ path: 'privacy', component: PrivacyPolicy },
 { path: '**', redirectTo: '' }
 ];
