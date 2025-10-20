2a6e237066078b94239edb7abe819a3664ab282d
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-10-20 15:11:57 +0200

    edit for skill image

--- src/app/components/main-contact/skills/skills.ts
+++ src/app/components/main-contact/skills/skills.ts
@@ -17,56 +17,56 @@ export class SkillsComponent {
   }[] = [
     {
     
-      icon : 'assets/icon_logos/html.png',
+      icon : './assets/icon_logos/html.png',
       text : 'HTML'
     },
     {
-    
-      icon : 'assets/icon_logos/16. Tech icons.png',
+
+      icon : './assets/icon_logos/16. Tech icons.png',
        text : 'CSS',
 
     },
     {
-      
-      icon : 'assets/icon_logos/Javascript.png',
+
+      icon : './assets/icon_logos/Javascript.png',
        text : 'JavaScript'
     },
     {
 
-      icon : 'assets/icon_logos/Frame.png',
+      icon : './assets/icon_logos/Frame.png',
        text : 'Material Design'
     },
     {
-     
-      icon : 'assets/icon_logos/Vector (1).png',
+
+      icon : './assets/icon_logos/Vector (1).png',
        text : 'TypeScript',
     },
     {
      
-      icon : 'assets/icon_logos/Angular.png',
+      icon : './assets/icon_logos/Angular.png',
        text : 'Angular'
     },
     {
-      icon : 'assets/icon_logos/16. Tech icons (1).png',
+      icon : './assets/icon_logos/16. Tech icons (1).png',
        text : 'Firebase'
 
     },
     {
       
-      icon : 'assets/icon_logos/16. Tech icons (2).png',
+      icon : './assets/icon_logos/16. Tech icons (2).png',
        text : 'Git'
     }
     ,{
       
-      icon : 'assets/icon_logos/Api.png',
+      icon : './assets/icon_logos/Api.png',
        text : 'REST-API'
     },
     {
-      icon :'assets/icon_logos/16. Tech icons (3).png',
+      icon : './assets/icon_logos/16. Tech icons (3).png',
        text : 'Scrum'
     },
     {
-      icon:'assets/icon_logos/16. Tech icons (4).png',
+      icon:'./assets/icon_logos/16. Tech icons (4).png',
        text : 'Growth mindset'
     }
   ]
